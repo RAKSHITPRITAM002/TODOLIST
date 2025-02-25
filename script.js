df3d24ed4bb60907022b6be6e7001a74328c6c6a
RAKSHITPRITAM002 <rakshitpritam002@gmail.com>
2025-02-25 21:09:53 +0530

    update

--- script.js
+++ script.js
@@ -13,12 +13,23 @@ function addtask(){
         li.appendChild(span);
     }
     inputbox.value="";
+    savedata();
 }
 listcontainer.addEventListener("click",function(e){
     if(e.target.tagName === "LI"){
         e.target.classList.toggle("checked");
+        savedata();
     }
     else if(e.target.tagName === "SPAN"){
         e.target.parentElement.remove();
+        savedata();
     }
 }, false);
+
+function savedata(){
+    localStorage.setItem("data",list-container.innerHTML);
+}
+function showTask(){
+listcontainer.innerHTML=localStorage.getItem("data");
+}
+showTask();
